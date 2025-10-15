1ab96f4326267e58ae6faaefb1d68250270031c7
luzhang <luzhang@nvidia.com>
2025-10-14 19:01:25 -0700

    use the correct hostname for https and websocket url

--- constants/constants.tsx
+++ constants/constants.tsx
@@ -1,4 +1,4 @@
 export const botHeader = 'Scout Bot'
 // Default API URLs for fallback
-export const DEFAULT_CHAT_COMPLETION_URL = 'https://tpm-ai-assistant-nat.prd.astra.nvidia.com/chat/stream';
-export const DEFAULT_WEBSOCKET_URL = 'wss://tpm-ai-assistant-nat.prd.astra.nvidia.com/websocket';
+export const DEFAULT_CHAT_COMPLETION_URL = 'https://tpm-nat.prd.astra.nvidia.com/chat/stream';
+export const DEFAULT_WEBSOCKET_URL = 'wss://tpm-nat.prd.astra.nvidia.com/websocket';
